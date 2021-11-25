54b861039d6ad0eff2e01b54c75a1a4aebeafaba
MelvDouc <melv.douc@gmaiL.com>
2021-11-25 22:29:09 +0100

    change layout with sidebar

--- cours_adultes_thionville_echecs/settings.py
+++ cours_adultes_thionville_echecs/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 from decouple import config
 
@@ -37,10 +36,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    "django.contrib.admin",
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    "django.contrib.contenttypes",
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "main.apps.MainConfig"
